eecbfc427c9f419a5dd9fb08d8bc90ed142b35f9
nmsn <136696700@qq.com>
2022-11-30 22:14:06 +0800

    feat: update script

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const path = require("path");
 const fs = require("fs");
 
-const issuesUrl = "https://api.github.com/repos/nmsn/blog/issues";
+const issuesUrl = "https://api.github.com/repos/nmsn/blog/issues?per_page=100";
 
 const formatTitle = ({ title, level }) => {
   return `${"#".repeat(level)} ${title}`;
@@ -20,14 +20,23 @@ const formatTags = (tags) => {
   return `${tags.map((item) => formatTag(item))}`;
 };
 
-const getTableContentItem = ({ url, title, tags }) => {
-  return `|${formatLink({ url, title })}|${formatTags(tags)}|`;
+const getTableContentItem = ({
+  url,
+  title,
+  tags,
+  created_at,
+  updated_at,
+  comments,
+}) => {
+  return `|${formatLink({ url, title })}|${formatTags(
+    tags
+  )}|${created_at}|${updated_at}|${comments}|`;
 };
 
 const getTableContent = (origin) => {
   const content = origin?.map((item) => getTableContentItem(item)).join("\n");
 
-  return `|标题|类型|\n|---|---|\n${content}`;
+  return `|标题|类型|创建时间|更新时间|评论数|\n|---|---|---|---|---|\n${content}`;
 };
 
 const updateTime = () => {
@@ -38,9 +47,9 @@ axios(issuesUrl).then((res) => {
   const { data = [] } = res || {};
 
   const origin = data.map((item) => {
-    const { title, html_url, labels } = item;
+    const { title, html_url, labels, created_at, updated_at, comments } = item;
     const tags = labels?.map((item) => item.name);
-    return { title, url: html_url, tags };
+    return { title, url: html_url, tags, created_at, updated_at, comments };
   });
 
   const content = getTableContent(origin);
